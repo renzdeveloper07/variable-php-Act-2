75379b79d11f6f785134c1b91d3abe27d6ed465c
RenzDev <renzbenablo@gmail.com>
2025-08-13 01:39:03 +0000

    Changes Commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+<body>
+        <?php include 'main.php'; ?>
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,15 @@
+<?php
+$name = "Benablo Renz Boy M> ";
+$age = 19;
+$school = "Global Reciprocal College";
+$course = "BSIT";
+$subject = "Websys";
+$student = true;
+
+echo "name: = $name\n";
+echo "age: = $age\n";
+echo "School: = $school\n";
+echo "Course: = $course\n";
+echo "Subject; = $subject\n";
+echo "Student: = " . ($student  ? "Yes" : "No"). "\n<br>";
+?>
